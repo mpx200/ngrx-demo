78c5d67d4c295641f58f28f3d6aa0eee26b12afe
Ivan Stepic <ivan.stepic@comtrade.com>
2017-12-23 12:37:30 +0100

    adding routes

--- ngrx-demo/src/app/app.module.ts
+++ ngrx-demo/src/app/app.module.ts
@@ -3,12 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MeetUpComponent } from './meet-up';
-import { ParticipantsListComponent, ParticipantsEditComponent } from './participants';
+import {
+  ParticipantsListComponent,
+  ParticipantsEditComponent
+} from './participants';
 import { NgRxMeetupCommonModule } from '../common';
-
+import { RouterModule } from '@angular/router';
+import { routes } from './app.router';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [AppComponent, MeetUpComponent, ParticipantsListComponent, ParticipantsEditComponent],
-  imports: [BrowserModule, NgRxMeetupCommonModule],
+  declarations: [
+    AppComponent,
+    MeetUpComponent,
+    ParticipantsListComponent,
+    ParticipantsEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    NgRxMeetupCommonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ ngrx-demo/src/app/app.router.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import {
+  ParticipantsEditComponent,
+  ParticipantsListComponent
+} from './participants';
+import { MeetUpComponent } from './meet-up';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/meetup', pathMatch: 'full' },
+  { path: 'meetup', component: MeetUpComponent },
+  { path: 'meetup/part/:id', component: ParticipantsEditComponent },
+  { path: 'meetup/part', component: ParticipantsListComponent }
+];
+
